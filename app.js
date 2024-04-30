f0118a395d8c40c6e8ebed76ec039d07f56cee60
Krrish Halder <krrish.code07@gmail.com>
2024-04-30 16:28:25 +0530

    Update .gitignore and package.json to include dotenv dependency and use environment variables in app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/blogify');
+mongoose.connect(process.env.DATABASE_URL);
 
 const blogPostRoutes = require('./routes/blogPostRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 app.use('/api/blogposts', blogPostRoutes);
+app.use('/api/users', userRoutes);
 
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
